af001ad4be867ebcb3336e3889dbc3e9abe9290c
corruptionbangla24-ops <corruptionbangla24@gmail.com>
2026-05-27 06:09:23 +0600

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // 🎰 [উইনগো কালার ট্রেড ওরিজিনাল ডোমেইন সিঙ্ক]
-const MAIN_SITE_URL = "https://onrender.com"; 
+const MAIN_SITE_URL = "https://betlover247.onrender.com"; 
 
 // 🧠 [অফিশিয়াল গেম মেমোরি সেশন ট্র্যাকার লক ভাই ভাই]
 let activeMinesSessions = {};
